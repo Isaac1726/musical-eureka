6da58280491526ec5af2341de188cd1941c2e891
kvaskov <68589260+kvaskov@users.noreply.github.com>
2023-02-05 16:12:29 +0400

    Update streamlit_sharing.py

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -1,19 +1,31 @@
 import streamlit as st
 import py3Dmol
+import requests
 from stmol import showmol
+import json
+
+def get_protein_info(prot):
+    req = requests.get(f'https://data.rcsb.org/rest/v1/core/entry/{prot}/')
+    prot_data = json.loads(req.text)
+    title = prot_data["struct"]["title"]
+    descriptor = prot_data["struct"]["pdbx_descriptor"]
+    return descriptor, title
+
 st.sidebar.title('Show Proteins')
-prot_str='1A2C,1BML,1D5M,1D5X,1D5Z,1D6E,1DEE,1E9F,1FC2,1FCC,1G4U,1GZS,1HE1,1HEZ,1HQR,1HXY,1IBX,1JBU,1JWM,1JWS'
-prot_list=prot_str.split(',')
-bcolor = st.sidebar.color_picker('Pick A Color', '#00f900')
-protein=st.sidebar.selectbox('select protein',prot_list)
-style = st.sidebar.selectbox('style',['line','cross','stick','sphere','cartoon','clicksphere'])
+bcolor = st.sidebar.color_picker('Pick A Color', '#DBDEDB')
+protein = st.sidebar.text_input('Enter protein:', "")
+style = st.sidebar.selectbox('style',['stick','line','cross','sphere','cartoon','clicksphere'])
 spin = st.sidebar.checkbox('Spin', value = False)
-xyzview = py3Dmol.view(query='pdb:'+protein)
-xyzview.setStyle({style:{'color':'spectrum'}})
-xyzview.setBackgroundColor(bcolor)
-if spin:
-    xyzview.spin(True)
-else:
-    xyzview.spin(False)
-xyzview.zoomTo()
-showmol(xyzview,height=500,width=800)
+if protein != "":
+    st.sidebar.markdown(f'**{get_protein_info(protein)[0]}**')
+    st.sidebar.markdown(f'**{get_protein_info(protein)[1]}**')
+    xyzview = py3Dmol.view(query='pdb:'+protein)
+    xyzview.setStyle({style:{'color':'spectrum'}})
+    xyzview.setBackgroundColor(bcolor)
+    if spin:
+        xyzview.spin(True)
+    else:
+        xyzview.spin(False)
+    xyzview.zoomTo()
+    showmol(xyzview,height=500,width=800)
+
